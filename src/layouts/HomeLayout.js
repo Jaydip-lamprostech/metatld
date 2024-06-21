04d2d6debf49fbe4e4e5fe7ccb2e3e15bf1d795a
Jaydip-lamprostech <jaydip.lamprostech@gmail.com>
2024-06-21 17:09:18 +0530

    modify layouts

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -1,39 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import HomePageHeader from "../components/HomePageHeader";
+import HomePageFooter from "../components/HomePageFooter";
 
 function HomeLayout() {
   return (
     <div>
-      {/* <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/search">Search Domain or TLD</Link>
-            </li>
-            <li>
-              <Link to="/register/tld">Register TLD</Link>
-            </li>
-            <li>
-              <Link to="/register/domain">Register Domain</Link>
-            </li>
-            <li>
-              <Link to="/user/sample-address">User Profile</Link>
-            </li>
-            <li>
-              <Link to="/name-services">Name Services</Link>
-            </li>
-            <li>
-              <Link to="/domain/sample-domain">Single Domain</Link>
-            </li>
-            <li>
-              <Link to="/tld/sample-tld">Single TLD</Link>
-            </li>
-          </ul>
-        </nav>
-      </header> */}
+      <div>
+        <HomePageHeader />
+        <main>
+          <Outlet />
+        </main>
+        <HomePageFooter />
+      </div>
     </div>
   );
 }
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
 
 function MainLayout() {
-  return <div>MainLayout</div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default MainLayout;
